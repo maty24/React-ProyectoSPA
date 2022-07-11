d6e13b066fc7ca82a4c951902d26a599abe46582
MatiasG <mgodoy@aligare.cl>
2022-07-10 21:32:33 -0400

    init P

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HeroesApp } from './HeroesApp'
+import './style.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <HeroesApp />
+)
